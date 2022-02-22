d0a7afc443218468e0cefe93681b045c01a58549
point77 <o_b_a@ukr.net>
2022-02-22 15:19:37 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// const window = document.window;
+// const head = document.head;
+// const html = document.html;
+// const body = document.body;
+// let header = document.querySelectorAll("header");
+
+// let a = 1, b = 'abc';
+
+// body.header.addEventListener('click', function(){
+// 	header.classList.toggle('show');
+// 	console.log(header);
+// })
